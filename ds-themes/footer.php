051e974724124e1d367398ad6457dc592ef4b16f
liron729 <lironaliu8@gmail.com>
2025-11-20 17:34:44 +0100

    /..

---
+++ ds-themes/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>digital school</p>
+    </footer>
+</body>
+</html>
---
+++ ds-themes/functions.php
@@ -0,0 +1,9 @@
+<?php
+function load_sctripts(){
+wp_enqueue_style('style', get_stylesheet_uri());
+
+wp_enqueue_script('dropdown', get_template_directory_uri(). '/js/dropdown.js', array(), '1.0', false);
+
+}
+add_action('wp_enqueue_scripts', 'load_scripts');
+?>
